b8a1333c1152b7811a94e73b3d1b78c2677441f6
MEGA-MAN-EXE\wolfe <mxv647@med.miami.edu>
2018-04-16 16:09:16 -0400

    bare minimum for submission met, exlcuding README

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -44,41 +44,26 @@ connection.connect(function(err) {
  	 	.then(function(answer){
  			var query = "SELECT * FROM products WHERE ?";
  			connection.query(query, {item_id : answer.item_id}, function(err, res) {
- 				//compare order quantity vs. stock
- 					//if not enough, return to showroom
- 				if ( answer.stock_quantity > res[0].stock_quantity) {
+        var cost = res[0].price;
+        var updateStock = res[0].stock_quantity - answer.stock_quantity;
+ 				//compare order quantity vs. stock; if not enough, return to showroom
+ 				if (answer.stock_quantity > res[0].stock_quantity) {
  					console.log("\n Insufficient quantity!\n");
  					return showroom();
  				}
- 					//else, update SQL, show cost
- 				else {
- 				console.log("You've selected product ID " + res[0].item_id +
- 				 ", " + res[0].product_name + " for $" 
- 				+ res[0].price);
- 				}
- 			//QuantitySelect(); 
+        else{
+          var query2 = "UPDATE products SET ? WHERE ?";
+          connection.query(query2, 
+          [
+          {stock_quantity : updateStock}
+          ,
+          {item_id : answer.item_id}
+          ], 
+          function(err, res){
+            var total = answer.stock_quantity * cost;
+            console.log("Thank you for your purchase! Your total will be $" + total)
+         })          
+        }
  			})
  		})
  }
-
-
-
-
-
-
-
-//update
-//  function QuantitySelect() {
-//  			inquirer.prompt({
-//  			name: "stock_quantity",
-//  			type: "input",
-//  			message: "How many would you like to buy?"
-//  			}).then(function(answer){
-// 			var query = "SELECT * FROM products WHERE ?";
-//  			connection.query(query, {stock_quantity : answer.item_id}, function(err, res) {
-//  				console.log("You've selected product ID " + res[0].item_id +
-//  				 ", " + res[0].product_name + " for $" 
-//  				+ res[0].price);
-//  			QuantitySelect();  				
-//  			})
-// }
